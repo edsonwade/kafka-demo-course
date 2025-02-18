63b9a2c83c8541553d93ff0b86070744838d2c8a
vanilson muhongo <vanilsonmuhongo@gmail.com>
2025-02-18 19:50:11 +0000

    feat: Add Kafka producer and consumer configurations and examples
    feat: Add Kafka producer with key example
    
    - Implemented a Kafka producer that sends messages with keys to ensure messages with the same key are sent to the same partition.
    - Added detailed explanation and code examples in the README file.
    - Included configuration for producer properties, message sending, logging, and exception handling.
    - Explained round-robin and sticky partitioning techniques.

---
+++ kafka-basic/src/main/java/code/with/vanilson/ProducerDemoKeys.java
@@ -0,0 +1,64 @@
+package code.with.vanilson;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ * ProducerDemoKeys
+ *
+ * @author vamuhong
+ * @version 1.0
+ * @since 2025-02-18
+ */
+public class ProducerDemoKeys {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getName());
+
+    public static void main(String[] args) {
+        Properties properties = getProperties();
+        // Criar uma mensagem para enviar para o tópico Kafka. A mensagem é composta por um valor e uma chave.
+        // A chave é usada para garantir que a mensagem com a mesma chave seja enviada para a mesma partição.
+        // Se a chave for nula, o Kafka escolherá aleatoriamente uma partição para enviar a mensagem.
+        // Se a chave não for nula, o Kafka garantirá que todas as mensagens com a mesma chave sejam enviadas para a mesma partição.
+        // Para melhorar o desempenho, o produtor usa o que é chamado de sticky partitioning, onde várias mensagens são enviadas para a mesma partição até que um certo limite seja atingido, antes de mudar para outra partição.
+        // O particionador Round-Robin é usado para distribuir as mensagens de forma equilibrada entre todas as partições disponíveis.
+        // O particionador Round-Robin é o particionador padrão do Kafka.
+        // O particionador Round-Robin é usado quando a chave é nula.
+        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+            // Enviar 10 mensagens para o tópico Kafka chamado "demo_java". Isto é que chamamos sticky partitioning,
+            // onde a mesma chave é enviada para a mesma partição.
+            for (int j = 0; j < 2; j++) {
+                for (int i = 0; i <= 10; i++) {
+                    String topic = "demo_java";
+                    String key = "truck_id " + i;
+                    String value = "Hello world " + i;
+                    ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+                    producer.send(record, (recordMetadata, e) -> {
+                        if (e == null) {
+                            log.info("key :{}", key + "| Partition:{} " + recordMetadata.partition());
+                        } else {
+                            log.error("Error while sending message to Kafka {} ", e.getMessage());
+                        }
+                    });
+                }
+                Thread.sleep(500);
+                producer.flush();
+            }
+        } catch (InterruptedException e) {
+            log.error("Thread exception error ", e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        return properties;
+    }
+}
