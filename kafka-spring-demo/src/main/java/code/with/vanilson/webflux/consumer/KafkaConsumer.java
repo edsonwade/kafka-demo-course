c701d804d228ade8e3553c437db3f13e65aeaa8c
vanilson muhongo <vanilsonmuhongo@gmail.com>
2025-02-24 19:33:58 +0000

    feat:Impplemented producer and consumer
    - Created a srping boot kafka stream event
     demo app to send and receive data.
    
    - key properties:
      1. write msg with json form producer KafkaTemplate<String, Student>
      2.NewTopic that allows to create a new topic use ToppicBuilder
      3. read messaga consumer with use @KafkaListener(topics,groupId);

--- kafka-spring-demo/src/main/java/code/with/vanilson/webflux/consumer/KafkaConsumer.java
+++ kafka-spring-demo/src/main/java/code/with/vanilson/webflux/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package code.with.vanilson.webflux.consumer;
 
+import code.with.vanilson.webflux.payload.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "twitter-topic", groupId = "MyGroup")
+    //@KafkaListener(topics = "twitter-topic", groupId = "MyGroup")
     public void consumeMessage(String message) {
         log.info("Consuming message:{}  from topic: {}", message, "twitter-topic");
+    }
+
+    @KafkaListener(topics = "json-topic", groupId = "MyGroup")
+    public void consumeJsonMessage(Student message) {
+        log.info("Consuming json message :{}  from topic: {}", message.toString(), "json-topic");
     }
 
 }
--- kafka-spring-demo/src/main/java/code/with/vanilson/webflux/rest/MessagerController.java
+++ kafka-spring-demo/src/main/java/code/with/vanilson/webflux/rest/MessagerController.java
@@ -1,5 +1,7 @@
 package code.with.vanilson.webflux.rest;
 
+import code.with.vanilson.webflux.payload.Student;
+import code.with.vanilson.webflux.producer.KafkaJsonProducer;
 import code.with.vanilson.webflux.producer.KafkaProducer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,19 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2025-02-24
  */
 @RestController
-@RequestMapping(path = "/send-message")
+@RequestMapping(path = "/message")
 public class MessagerController {
 
     private final KafkaProducer kafkaProducer;
+    private final KafkaJsonProducer producer;
 
-    public MessagerController(KafkaProducer kafkaProducer) {
+    public MessagerController(KafkaProducer kafkaProducer, KafkaJsonProducer producer) {
         this.kafkaProducer = kafkaProducer;
+        this.producer = producer;
     }
 
     @PostMapping
     public ResponseEntity<String> sendMessage(@RequestBody String message) {
         kafkaProducer.sendMessage(message);
         return ResponseEntity.ok("Message Queued Successfully");
+
+    }
+
+    @PostMapping(value = "/student")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody Student student) {
+        producer.sendJsonMessage(student);
+        return ResponseEntity.ok("Message sent to kafka topic");
 
     }
 }
